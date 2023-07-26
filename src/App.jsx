25ed18c11f046c9ae9343cd9eed63542844a0f73
Luis Uzcategui <alfonsouzcategui2@gmail.com>
2023-07-26 07:50:59 -0500

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,26 @@
 import { useState } from 'react'
 
-import { HashRouter, Routes, Route } from 'react-roter-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 
-import ProtectedRouter from './components/ProtectedRouter';
+//import ProtectedRouter from './components/ProtectedRouter';
 import Home from "./pages/Home"
 import Login from "./pages/Login"
 import './App.css'
+import AddPublication from './components/AddPublication'
 
 function App() {
 
   return (
     <HashRouter>
       <Routes>
-        <Route path="/login" element={<Login/>}/>
-        <Route element={<ProtectedRouter/>}>
-          <Route path="/home" element={<Home/>}/>
+        <Route path='/login' element={<Login />} />
+        <Route path='/add' element={<AddPublication />} />
+        <Route element={'<ProtectedRouter/>'}>
+          <Route path='/home' element={<Home />} />
         </Route>
       </Routes>
     </HashRouter>
-  )
+  );
 }
 
 export default App
--- src/components/AddPublication.jsx
+++ src/components/AddPublication.jsx
@@ -1,19 +1,47 @@
 import { useForm } from 'react-hook-form'
 
+import { marcas, modelos, vga, cuotas, rango } from '../data/data.js';
+
 const AddPublication = () => {
   const { handleSubmit, register, reset } = useForm();
   
   const submit = data => console.log(data)
   return (
     <form onSubmit={handleSubmit(submit)}>
-      <select {...register('marcas')}>
-        <option></option>
+      <ul>
+        {marcas.map((marca, i) => (
+          <li key={i}>{marca}</li>
+        ))}
+      </ul>
+      <ul>
+        {modelos.map((modelo, i) => (
+          <li key={i}>{modelo}</li>
+        ))}
+      </ul>
+      <select>
+        {vga.map((valor, i) => (
+          <option key={i} value={i}>
+            {valor}
+          </option>
+        ))}
       </select>
-      <button>
-        Login
-      </button>
+      <select>
+        {cuotas.map((valor, i) => (
+          <option key={i} value={i}>
+            {valor}
+          </option>
+        ))}
+      </select>
+      <select>
+        {rango.map((valor, i) => (
+          <option key={i} value={i}>
+            {valor}
+          </option>
+        ))}
+      </select>
+      <button>Login</button>
     </form>
-  )
+  );
 }
 
 export default AddPublication;
